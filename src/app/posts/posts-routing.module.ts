95939d098ba85c540fd181cc8dc16061a445ad92
iballi2017 <iballi201@gmail.com>
2024-07-23 11:59:11 +0100

    update

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './posts.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PostsComponent,
+    children: [
+      {
+        path: '',
+        component: PostListComponent,
+      },
+      {
+        path: 'list',
+        component: PostListComponent,
+      },
+      {
+        path: 'new',
+        component: AddPostComponent,
+      },
+    ],
   },
 ];
 
