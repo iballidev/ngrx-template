9c75e09617ca9f53f26dbd504c86d137f9e6424a
iballi2017 <iballi201@gmail.com>
2024-07-24 08:41:50 +0100

    update

--- src/app/posts/add-post/add-post.component.ts
+++ src/app/posts/add-post/add-post.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
@@ -9,16 +10,25 @@ import {
 @Component({
   selector: 'app-add-post',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.scss',
 })
 export class AddPostComponent implements OnInit {
   addPostForm!: FormGroup;
+  label: any;
+  imgUrl: any;
+  isReadyToBeSaved!: boolean;
+  isFile!: boolean;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.buildForm();
+
+    console.log('this.label: ', this.label);
+    console.log('this.imgUrl: ', this.imgUrl);
+    console.log('this.isReadyToBeSaved: ', this.isReadyToBeSaved);
+    console.log('this.isFile: ', this.isFile);
   }
 
   buildForm() {
@@ -29,8 +39,34 @@ export class AddPostComponent implements OnInit {
     });
   }
 
-  onSubmit(data:FormGroup){
-    console.log("data: ", data.value)
+  onChange($event: any) {
+    console.log('$event: ', $event);
+
+    let rawImg = $event.target.files[0];
+    console.log('rawImg: ', rawImg);
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(rawImg);
+    reader.onload = (event: any) => {
+      // console.log("reader.onload: ", event)
+      this.imgUrl = event.target.result;
+      this.label = rawImg.name;
+      this.isReadyToBeSaved = true;
+      this.isFile = true;
+      this.uploadImage();
+    };
+  }
+
+  uploadImage() {
+    console.log('this.label: ', this.label);
+    console.log('this.imgUrl: ', this.imgUrl);
+    console.log('this.isReadyToBeSaved: ', this.isReadyToBeSaved);
+    console.log('this.isFile: ', this.isFile);
+  }
+
+  onSubmit(data: FormGroup) {
+    console.log('data: ', data.value);
   }
 
   backHistory() {
