9230321d79c096b48925e7c023d0e9577e758344
iballidev <iballi.dev@gmail.com>
2024-07-25 13:42:23 +0100

    update

--- src/app/todos/todosStore/todos.selector.ts
+++ src/app/todos/todosStore/todos.selector.ts
@@ -1,16 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { TodosState } from './todos.state';
 
-// export interface AppState {
-//   todos: TodoState;
-// }
-
-// export const selectFeature = (state: AppState) => state.todos;
-
 export const getTodosState = createFeatureSelector<TodosState>('todos');
 
 export const selectFeatureTodos = createSelector(
-  // selectFeature,
   getTodosState,
   (state: TodosState) => state.todos
 );
